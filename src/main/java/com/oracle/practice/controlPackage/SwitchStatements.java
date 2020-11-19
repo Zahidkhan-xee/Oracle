4a51c819d54360dedb18d0ecd6a2b1be61c553dc
Zahidkhan-xee <xahiidkhan@gmail.com>
2020-11-19 22:55:54 +0530

    Added ternary operator.

---
+++ src/main/java/com/oracle/practice/controlPackage/SwitchStatements.java
@@ -0,0 +1,41 @@
+package com.oracle.practice.controlPackage;
+
+import java.util.Scanner;
+
+public class SwitchStatements {
+    public static void main(String[] args) {
+        String day = new Scanner(System.in).next();
+
+        switch (day.toLowerCase()) {
+            case "monday":
+                System.out.println("First day of week");
+                break;
+            case "tuesday":
+                System.out.println("Second day");
+                break;
+            default:
+                System.out.println("Other day.");
+            break;
+        }
+        int x = day.length()%10;
+        switch (x){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 9:
+                System.out.println("ODD NUMBER");
+                break;
+            case 2:
+            case 4:
+            case 0:
+            case 6:
+            case 8:
+                System.out.println("Even Number");
+                break;
+            default:
+                System.out.println("Not in range");
+        }
+
+    }
+}
