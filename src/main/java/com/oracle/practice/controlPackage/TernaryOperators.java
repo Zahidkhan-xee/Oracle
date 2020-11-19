19a3e07b593d2bc1ba3e2f481b6497bd8e0d2499
Zahidkhan-xee <xahiidkhan@gmail.com>
2020-11-19 22:13:08 +0530

    Added ternary operator.

---
+++ src/main/java/com/oracle/practice/controlPackage/TernaryOperators.java
@@ -0,0 +1,7 @@
+package com.oracle.practice.controlPackage;
+
+public class TernaryOperators {
+    public static void main(String[] args) {
+        System.out.println(1<2 ? "hello": "world");
+    }
+}
