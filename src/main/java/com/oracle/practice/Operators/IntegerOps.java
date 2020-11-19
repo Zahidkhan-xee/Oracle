17c5a3f3cae1ed1de03fc9cea142c4fa77fe7ba2
Zahidkhan-xee <xahiidkhan@gmail.com>
2020-11-19 23:57:24 +0530

    Done with Operators lecture 3

--- src/main/java/com/oracle/practice/Operators/IntegerOps.java
+++ src/main/java/com/oracle/practice/Operators/IntegerOps.java
@@ -1,5 +1,21 @@
 package com.oracle.practice.Operators;
 
+import java.util.Random;
+
 public class IntegerOps {
+    public static void main(String[] args) {
+        Integer integer = 10;
+        Integer integer2 = 10;
+
+        if(integer == integer2)
+            System.out.println("Equal");
+
+        System.out.println( integer + 2);
+        System.out.println( integer - 2);
+        System.out.println( integer / 2);
+        System.out.println( integer % 4);
+
+
+    }
 
 }
