25b417ddf7b59b65f48072d0d6285ec5da7474bf
Zahidkhan-xee <xahiidkhan@gmail.com>
2020-12-20 17:22:09 +0530

    Added ArrayBinary Search

---
+++ src/main/java/com/oracle/practice/Arrays/ArrayBinarySearch.java
@@ -0,0 +1,22 @@
+package com.oracle.practice.Arrays;
+
+import java.util.Arrays;
+
+public class ArrayBinarySearch {
+        public static void main(String[] args) {
+                int[] array = {1,2,4,5,6,7,10,100};
+                char[] array2 = {'c','d','e','f','g'};
+                String[] array3 = {"Hello","how","Are","U"};
+
+                System.out.println(Arrays.binarySearch(array,10));
+                System.out.println(Arrays.binarySearch(array2,'d'));
+                System.out.println(Arrays.binarySearch(array3,"Are"));
+
+                Arrays.sort(array3);
+                System.out.println(Arrays.binarySearch(array3,"Are"));
+                System.out.println(Arrays.binarySearch(array3,"U"));
+
+
+
+        }
+}
